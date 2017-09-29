08d239f1be5be54c424215f6b243675acb68ba49
Prabodh <prs-prabodh@users.noreply.github.com>
2017-09-29 21:46:04 +0530

    Codechef Easy

---
+++ LIKECS01.cpp
@@ -0,0 +1,25 @@
+/**
+*	@author Prabodh Ranjan Swain
+*	Institution - ASET, New Delhi
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int test;
+	cin>>test;
+	while(test--){
+		string s;
+		cin>>s;
+		set<char> a;
+		for(int i=0;i<s.length();i++){
+            a.insert(s[i]);
+		}
+		if(a.size()!=s.length())
+            cout<<"yes"<<endl;
+        else
+            cout<<"no"<<endl;
+	}
+	return 0;
+}
