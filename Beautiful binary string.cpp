31153335a62c391d248fb2cc269ff377134f77f4
Prabodh <prs-prabodh@users.noreply.github.com>
2018-01-27 14:40:53 +0530

    Hackerrank Practise

---
+++ Beautiful binary string.cpp
@@ -0,0 +1,30 @@
+/**
+*	@author Prabodh Ranjan Swain
+*	Institution - ASET, New Delhi
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	string a;
+	cin>>a;
+	int i=0,c=0;
+	while(i<n){
+        if(a[i]=='0'){
+            i++;
+            if(a[i]=='1'){
+                i++;
+                if(a[i]=='0'){
+                    c++;
+                    i++;
+                }
+            }
+        }
+        else i++;
+	}
+	cout<<c<<endl;
+	return 0;
+}
