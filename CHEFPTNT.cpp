6f33b8886642783e93027bd64567c966c89e9cce
Prabodh <prs-prabodh@users.noreply.github.com>
2018-02-03 14:19:10 +0530

    February Long Challenge 2018

---
+++ CHEFPTNT.cpp
@@ -0,0 +1,60 @@
+/**
+*	@author Prabodh Ranjan Swain
+*	Institution - ASET, New Delhi
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int test;
+	cin>>test;
+	while(test--){
+            int n,m,x,k;
+            cin>>n>>m>>x>>k;
+            string s;
+            cin>>s;
+            int e=0,o=0,p=1,sum=0;
+            if(x==0){
+                cout<<"no"<<endl;
+                continue;
+            }
+            if(n/(min(x,k))<=m&&n<=k){
+                for(int i=0;i<s.length();i++){
+                    if(s[i]=='E')
+                        e++;
+                    else o++;
+                }
+                int req=min(x,k);
+                while(p<=m&&sum<n){
+                    int red=min(req,(n-sum));
+                    if(p%2){
+                        if(o>0)
+                            red=min(o,red);
+                        if(o>=red){
+                            sum+=red;
+                            o-=red;
+                        }
+                        p++;
+                    }
+                    else{
+                        if(e>0)
+                            red=min(e,red);
+                        if(e>=red){
+                            sum+=red;
+                            e-=red;
+                        }
+                        p++;
+                    }
+                }
+            }
+            else{
+                cout<<"no"<<endl;
+                continue;
+            }
+            if(sum>=n)
+                cout<<"yes"<<endl;
+            else cout<<"no"<<endl;
+	}
+	return 0;
+}
