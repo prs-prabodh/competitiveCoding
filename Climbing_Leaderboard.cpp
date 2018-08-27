72bf541763e7f8eabfa9fe1f6d5fc71709a8cba9
Prabodh <prs-prabodh@users.noreply.github.com>
2018-08-27 21:23:33 +0530

    Hackerrank Practise

---
+++ Climbing_Leaderboard.cpp
@@ -0,0 +1,23 @@
+/**
+*	@author Prabodh Ranjan Swain
+*	Institution - ASET, New Delhi
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  unsigned long n, m, i, tmp;
+  cin >> n;
+  stack<unsigned long> scores;
+  for (i = 0; i < n; ++i) {
+    cin >> tmp;
+    if (scores.empty() || scores.top() != tmp) scores.push(tmp);
+  }
+  cin >> m;
+  for (i = 0; i < m; ++i) {
+    cin >> tmp;
+    while (!scores.empty() && tmp >= scores.top()) scores.pop();
+    cout << (scores.size() + 1) << endl;
+  }
+  return 0;
+}
